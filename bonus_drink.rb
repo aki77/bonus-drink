f99ae7acbc790bde74483fda0b475a0786232d5b
aki <lala.akira@gmail.com>
2015-10-05 15:17:49 +0900

    Implement total_count_for

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,13 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    total_count = amount
+
+    while amount >= 3
+      bonus_count = amount / 3
+      amount = amount % 3 + bonus_count
+      total_count += bonus_count
+    end
+
+    total_count
   end
 end
